8c20fd69743406e7df94d9541567667281e50e4b
chenjie520 <39550386+chenjie520@users.noreply.github.com>
2020-07-21 20:38:28 +0800

    添加单元测试

--- dashboard/src/main/java/com/net/dashboard/DashboardApplication.java
+++ dashboard/src/main/java/com/net/dashboard/DashboardApplication.java
@@ -3,7 +3,6 @@ package com.net.dashboard;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication()
 @MapperScan("com.net.dashboard.dao")
--- dashboard/src/main/java/com/net/dashboard/util/StringUtil.java
+++ dashboard/src/main/java/com/net/dashboard/util/StringUtil.java
@@ -1,9 +1,27 @@
 package com.net.dashboard.util;
 
+import java.util.UUID;
+
 /**
  *@Author chen_jie
  *@Version 1.0
  *@time 2020/7/21
  **/
 public class StringUtil {
+
+    /**
+    *   判断字符串是否为空，如果为空返回true,不为空返回false
+    */
+    public boolean isNull(String str){
+        return str == null || "".equals(str);
+    }
+
+    /**
+    * 获取32位的uuid
+    */
+    public String getUUID(){
+        return UUID.randomUUID().toString().replace("-","");
+    }
+
+
 }
