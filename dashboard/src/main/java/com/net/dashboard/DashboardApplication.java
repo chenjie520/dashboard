537fe408c7cc3c098395b624221351526067958f
chenjie520 <39550386+chenjie520@users.noreply.github.com>
2020-07-21 08:51:10 +0800

    Add files via upload
    添加springboot项目

---
+++ dashboard/src/main/java/com/net/dashboard/DashboardApplication.java
@@ -0,0 +1,14 @@
+package com.net.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class DashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DashboardApplication.class, args);
+    }
+
+}
