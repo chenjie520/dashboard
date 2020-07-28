7b0df6f8aca9372081dbf572fbc86f308ed64c19
chenjie520 <39550386+chenjie520@users.noreply.github.com>
2020-07-28 16:29:01 +0000

    创建折扣表

--- dashboard/src/main/resources/createTable.sql
+++ dashboard/src/main/resources/createTable.sql
@@ -21,3 +21,14 @@ CREATE TABLE IF NOT EXISTS `order`(
 	 `netnut_coustomer_id` varchar(128) comment 'netnut生成的coustomerId',
 	 PRIMARY key(`id`)
 );
+--创建折扣表
+CREATE TABLE IF NOT EXISTS `discount`(
+   `id` INT UNSIGNED AUTO_INCREMENT COMMENT 'id',
+   `number` VARCHAR(100)  COMMENT '折扣码',
+   `type` varchar(10) COMMENT '折扣力度(比如9折，则0.9)',
+	 `due_date` Date comment '到期时间',
+	 `dc_id` varchar(100) comment '使用人的dcId',
+	 `create_date` date comment '创建时间',
+	 `use_date` date comment '使用时间',
+	 PRIMARY key(`id`)
+);
