5af2656b07030f93d79979557339b65c6dc536c1
chenjie520 <39550386+chenjie520@users.noreply.github.com>
2020-07-28 15:15:20 +0000

    Update AuthController.java

--- dashboard/src/main/java/com/net/dashboard/controller/AuthController.java
+++ dashboard/src/main/java/com/net/dashboard/controller/AuthController.java
@@ -2,6 +2,7 @@ package com.net.dashboard.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import com.net.dashboard.config.ContentKey;
+import com.net.dashboard.dao.IUserDao;
 import com.net.dashboard.util.HttpClientUtil;
 import org.apache.oltu.oauth2.client.request.OAuthClientRequest;
 import org.apache.oltu.oauth2.client.response.OAuthAuthzResponse;
@@ -11,12 +12,15 @@ import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.javacord.api.DiscordApi;
 import org.javacord.api.DiscordApiBuilder;
 import org.javacord.api.entity.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
@@ -30,6 +34,9 @@ import java.util.concurrent.CompletableFuture;
 @Controller
 @RequestMapping("auth")
 public class AuthController {
+    @Autowired
+    private IUserDao userDao;
+
     @GetMapping("/login")
     public String login(){
         return "";
@@ -52,7 +59,7 @@ public class AuthController {
     }
 
     @RequestMapping("/callback")
-    public void getToken(HttpServletRequest request, Model model){
+    public void getToken(HttpServletRequest request, Model model, HttpServletResponse response){
         try {
             OAuthAuthzResponse oAuthAuthzResponse=OAuthAuthzResponse.oauthCodeAuthzResponse(request);
             Map<String,String> map=new HashMap<>();
@@ -74,18 +81,52 @@ public class AuthController {
             //user dcid
             String dcId=userMap.get("id").toString();
             String dcName=userMap.get("username").toString()+"#"+userMap.get("discriminator").toString();
-            System.out.println(data);
-            if(judgeUserStatus(dcId)){
-                System.out.println("当前用户在dcserver中");
+            com.net.dashboard.pojo.User deUser=new com.net.dashboard.pojo.User();
+            deUser.setDcId(dcId);
+            //找到这个dcid的用户信息
+            //如果两个相同用户同时登陆会报错
+            com.net.dashboard.pojo.User  user1= userDao.selectUserByDcId(deUser);
+            if(user1!=null){
+                //找到则更新
+                if(judgeUserStatus(dcId)){
+                    user1.setIsInDc("1");
+                    //System.out.println("当前用户在dcserver中");
+                }else{
+                    user1.setIsInDc("2");
+                    //System.out.println("当前用户不在dcserver中");
+                }
+                //更新用户是否在dcserver中的状态
+                userDao.updateUserIsInDc(user1);
             }else{
-                System.out.println("当前用户不在dcserver中");
+                //没找到则新增
+                user1=new com.net.dashboard.pojo.User();
+                if(judgeUserStatus(dcId)){
+                    user1.setIsInDc("1");
+                    //System.out.println("当前用户在dcserver中");
+                }else{
+                    user1.setIsInDc("2");
+                    //System.out.println("当前用户不在dcserver中");
+                }
+                user1.setDcId(dcId);
+                user1.setCreateDate(new Date());
+                userDao.insertUser(user1);
             }
+            //返回前端用户信息，并存放在session中
+            Map<String,Object> hashMap=new HashMap<>();
+            hashMap.put("dcId",dcId);
+            hashMap.put("dcName",dcName);
+            hashMap.put("isInDc",judgeUserStatus(dcId));
+            JSONObject jsonObject=new JSONObject();
+            jsonObject.put("result",hashMap);
+            request.getSession().setAttribute("result",hashMap);
+            response.getWriter().write(jsonObject.toJSONString());
         } catch (OAuthProblemException e) {
             e.printStackTrace();
         }catch (Exception e){
             e.printStackTrace();
         }
     }
+
 
     /**
      * 通过dcId判断用户的状态
