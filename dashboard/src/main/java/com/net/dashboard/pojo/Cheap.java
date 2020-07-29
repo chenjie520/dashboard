351b937d960dea91727f4e1dc61b4bcaa8dd3e8b
chenjie520 <39550386+chenjie520@users.noreply.github.com>
2020-07-29 14:34:27 +0000

    添加cheap

---
+++ dashboard/src/main/java/com/net/dashboard/pojo/Cheap.java
@@ -0,0 +1,12 @@
+package com.net.dashboard.pojo;
+
+import lombok.Data;
+
+
+@Data
+public class Cheap {
+    private String username;
+    private int balance;
+    private String proxy_authkey;
+    private String date_created;
+}
